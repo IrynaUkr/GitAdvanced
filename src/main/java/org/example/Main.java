226fe9e8fc5cf77e243a39f2dd75f83a4d85c832
irinapodoprigora <ukrira@gmail.com>
2022-09-30 21:51:51 +0100

    changed main2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,9 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Calculator calculator = new Calculator("smart");
+        Calculator calculator2 = new Calculator("intelligent");
+        System.out.println(calculator2);
+        System.out.println(calculator);
     }
-    Calculator calculator = new Calculator("smart");
-    Calculator calculator2 = new Calculator("intelligent");
 }
