26f3d004b5c3558e6f2f121b864cd2a16949b41d
irinapodoprigora <ukrira@gmail.com>
2022-09-30 21:58:28 +0100

    CalculatorUtils doSomething was added

--- src/main/java/org/example/CalculatorUtils.java
+++ src/main/java/org/example/CalculatorUtils.java
@@ -4,4 +4,7 @@ public class CalculatorUtils {
     static char[] parseString(String string){
         return string.replaceAll(" ","").toCharArray();
     }
+    static void doSomething(){
+        System.out.println("do something");
+    }
 }
