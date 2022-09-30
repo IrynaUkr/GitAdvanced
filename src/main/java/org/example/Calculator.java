a427d5a2774c5b0c78013e0170b26784ef784e5d
irinapodoprigora <ukrira@gmail.com>
2022-09-30 19:22:23 +0100

    bad changes were added

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -1,6 +1,9 @@
 package org.example;
 
 public class Calculator {
+    static void printSomeName(){
+        System.out.println("name");
+    }
     public static void main(String[] args) {
         int r = 5;
     }
