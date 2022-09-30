760e846146307ff6d049685f790b10a3e7c258f6
irinapodoprigora <ukrira@gmail.com>
2022-09-30 21:22:41 +0100

    Constructor as added

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -1,7 +1,14 @@
 package org.example;
 
 public class Calculator {
+    private final String name;
+
+    public Calculator(String name) {
+        this.name = name;
+    }
+
     public static void main(String[] args) {
         int r = 5;
+        System.out.println(new Calculator("simple calc"));
     }
 }
