129b980183b3dd0a4b6159103bc7c1ae4ab7b006
irinapodoprigora <ukrira@gmail.com>
2022-09-30 21:50:15 +0100

    changed main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,4 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+    Calculator calculator = new Calculator("smart");
+    Calculator calculator2 = new Calculator("intelligent");
 }
