6ab4c5ce79bf863095181f064f9a58bacac811ce
irinapodoprigora <ukrira@gmail.com>
2022-09-30 19:23:22 +0100

    Revert "bad changes" commit
    This reverts commit a427d5a2774c5b0c78013e0170b26784ef784e5d.

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -1,9 +1,6 @@
 package org.example;
 
 public class Calculator {
-    static void printSomeName(){
-        System.out.println("name");
-    }
     public static void main(String[] args) {
         int r = 5;
     }
