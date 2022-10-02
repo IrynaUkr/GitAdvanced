6121e931915d4e96294e12f82d54422029fea48b
irinapodoprigora <ukrira@gmail.com>
2022-10-02 07:55:07 +0100

    CalculatorUtils parseString return int

--- src/main/java/org/example/CalculatorUtils.java
+++ src/main/java/org/example/CalculatorUtils.java
@@ -1,10 +1,26 @@
 package org.example;
 
 public class CalculatorUtils {
-    static char[] parseString(String string){
-        return string.replaceAll(" ","").toCharArray();
+    static int[] parseString(String string) {
+        int length = string.length();
+        char[] charArray = string.replaceAll(" ", "").toCharArray();
+        int[] res = new int[length];
+        for (int i = 0; i < length; i++) {
+            res[i] = charArray[i] - 48;
+        }
+        return res;
     }
-    static void doSomething(){
+
+    static void doSomething() {
+
         System.out.println("do something");
+    }
+
+    static int calc(char[] parsed) {
+        int result = 0;
+        for (Character c : parsed) {
+            result += c + 48;
+        }
+        return result;
     }
 }
